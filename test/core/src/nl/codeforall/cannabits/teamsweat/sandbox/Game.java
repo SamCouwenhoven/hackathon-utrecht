363f29a3dc8f6a3af60996d03d61303a208932e4
Walton Tseng <walgeton@gmail.com>
2019-12-13 00:36:37 +0100

    first skeleton

---
+++ test/core/src/nl/codeforall/cannabits/teamsweat/sandbox/Game.java
@@ -0,0 +1,46 @@
+package nl.codeforall.cannabits.teamsweat.sandbox;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Game implements Screen {
+
+    private Rectangle player1;
+    private Texture player1gfx;
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
