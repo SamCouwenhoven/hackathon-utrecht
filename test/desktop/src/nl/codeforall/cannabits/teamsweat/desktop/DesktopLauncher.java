ac981331b1bee05e6337cfca0ad78545397d4ef5
Sam Couwenhoven <samcouwenhoven@gmail.com>
2019-12-12 20:44:15 +0100

    added a drop game

--- test/desktop/src/nl/codeforall/cannabits/teamsweat/desktop/DesktopLauncher.java
+++ test/desktop/src/nl/codeforall/cannabits/teamsweat/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package nl.codeforall.cannabits.teamsweat.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import nl.codeforall.cannabits.teamsweat.Drop;
 import nl.codeforall.cannabits.teamsweat.MyGdxGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.title = "lyricsFinder";
+		config.width = 800;
+		config.height = 480;
+
+		new LwjglApplication(new Drop(), config);
 	}
 }
