a2c831849798f302bccd71baa80462d011a82dc9
Sam Couwenhoven <samcouwenhoven@gmail.com>
2019-12-13 02:15:21 +0100

    it's compiling great

--- test/core/src/nl/codeforall/cannabits/teamsweat/gameobjects/traps/FreezeTrap.java
+++ test/core/src/nl/codeforall/cannabits/teamsweat/gameobjects/traps/FreezeTrap.java
@@ -1,10 +1,11 @@
 package nl.codeforall.cannabits.teamsweat.gameobjects.traps;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import nl.codeforall.cannabits.teamsweat.gameobjects.Player;
 
 public class FreezeTrap extends Trap {
-    private static Texture FREEZE_TRAP_TEXTURE = new Texture("traps/ice.png");
+    private static Texture FREEZE_TRAP_TEXTURE = new Texture(Gdx.files.internal("traps/ice.png"));
 
     public FreezeTrap() {
         super(FREEZE_TRAP_TEXTURE);
