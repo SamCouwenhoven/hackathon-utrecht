2b7973d3bf08634f2869e0e1eed069be255fbf6b
Walton Tseng <walgeton@gmail.com>
2019-12-13 01:07:41 +0100

    added boundaries

--- test/core/src/nl/codeforall/cannabits/teamsweat/screens/GameScreen.java
+++ test/core/src/nl/codeforall/cannabits/teamsweat/screens/GameScreen.java
@@ -5,11 +5,22 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import nl.codeforall.cannabits.teamsweat.game.LyricsFinder;
+import nl.codeforall.cannabits.teamsweat.gameobjects.Player;
+
+import java.awt.*;
 
 public class GameScreen implements Screen {
 
+    private final int X_SCREENLIMIT = 500;
+    private final int Y_SCREENLIMIT = 150;
+    private final int SPRITESIZE = 64;
+
+    private Player player1;
+    private Player player2;
+
     private LyricsFinder game;
     private OrthographicCamera camera;
+
 
     public GameScreen(final LyricsFinder game) {
         this.game = game;
@@ -19,7 +30,8 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1000, 600);
 
-
+        player1 = new Player();
+        player2 = new Player();
     }
 
     @Override
@@ -36,8 +48,18 @@ public class GameScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();// draw in here
-        game.font.draw(game.batch, "this is the temp gamescreen ", 500, 150);
+        game.font.draw(game.batch, "this is the temp gamescreen ", X_SCREENLIMIT, Y_SCREENLIMIT);
         game.batch.end();
+
+        //Boundaries
+        if (player1.x < 0)
+            player1.x = 0;
+        if (player2.x < 0)
+            player2.x = 0;
+        if (player1.x > X_SCREENLIMIT-SPRITESIZE)
+            player1.x = X_SCREENLIMIT-SPRITESIZE;
+        if (player2.y > Y_SCREENLIMIT-SPRITESIZE)
+            player2.y = Y_SCREENLIMIT-SPRITESIZE;
 
     }
 
